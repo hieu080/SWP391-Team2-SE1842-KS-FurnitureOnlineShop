bbd9a94d32db6e1f5e3c52824b2d1d61a093591c
HELLO <phantrunghieu020103@gmail.com>
2024-05-19 00:26:06 +0700

    Innit project

--- src/java/DAL/DBContext.java
+++ src/java/DAL/DBContext.java
@@ -3,14 +3,18 @@ package DAL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class DBContext {
     protected Connection connect;
     public DBContext() {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("C:/Users/HELLO/OneDrive/Documents/NetBeansProjects/FurnitureWebShop/")  // Đường dẫn tới thư mục chứa file .env
+                .load();
         try {
-            String url = "jdbc:mysql://localhost:3306/furniture?useSSL=false";
-            String username = "root";
-            String password = "123456";
+            String url = dotenv.get("DB_URL");
+            String username = dotenv.get("DB_USER_NAME");
+            String password = dotenv.get("DB_PASSWORD");
             
             // Tạo kết nối
             connect = DriverManager.getConnection(url, username, password);

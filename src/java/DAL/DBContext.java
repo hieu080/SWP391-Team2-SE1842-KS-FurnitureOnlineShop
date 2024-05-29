3419f009f8cdab86f4889499e4a018c9dbb84732
DESKTOP-PHDV6FF\HELLO <phantrunghieu020103@gmail.com>
2024-05-29 20:39:56 +0700

    Tạo ProductDAO.java và tạo kết nối database DBContext.java

--- src/java/DAL/DBContext.java
+++ src/java/DAL/DBContext.java
@@ -4,10 +4,35 @@
  */
 package DAL;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author HELLO
  */
 public class DBContext {
-    
+
+    protected Connection connect;
+
+    public DBContext() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/furniturewebshop";
+            String username = "root";
+            String password = "";
+
+            // Tạo kết nối
+            connect = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
 }

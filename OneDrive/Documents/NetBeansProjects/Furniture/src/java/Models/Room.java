1a9a091889b4a63dc888598cef2ed34f120651b1
HELLO <phantrunghieu020103@gmail.com>
2024-05-29 13:20:01 +0700

    Tạo đối tượng Room.java

--- OneDrive/Documents/NetBeansProjects/Furniture/src/java/Models/Room.java
+++ OneDrive/Documents/NetBeansProjects/Furniture/src/java/Models/Room.java
@@ -9,5 +9,31 @@ package Models;
  * @author HELLO
  */
 public class Room {
+    private int id;
+    private String roomname;
+
+    public Room() {
+    }
+
+    public Room(String roomname) {
+        this.roomname = roomname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRoomname() {
+        return roomname;
+    }
+
+    public void setRoomname(String roomname) {
+        this.roomname = roomname;
+    }
+    
     
 }
